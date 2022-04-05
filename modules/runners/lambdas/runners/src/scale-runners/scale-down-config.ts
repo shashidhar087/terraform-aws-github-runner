6003ef7b5cda536eb13f7f67e8b3383d7070c9ec
vinayakas <shashidhar.vinayaka@dowjones.com>
2022-04-05 13:00:58 -0400

    fix: fix InPeriod issue #1889

--- modules/runners/lambdas/runners/src/scale-runners/scale-down-config.ts
+++ modules/runners/lambdas/runners/src/scale-runners/scale-down-config.ts
@@ -13,8 +13,28 @@ function inPeriod(period: ScalingDownConfig): boolean {
   const expr = parser.parseExpression(period.cron, {
     tz: period.timeZone,
   });
-  const next = moment(expr.next().toDate());
-  return Math.abs(next.diff(now, 'seconds')) < 5; // we keep a range of 5 seconds
+  const expr_second = expr.fields.second.toString();
+  const expr_minute = expr.fields.minute.toString();
+  const expr_hour = expr.fields.hour.toString();
+  const expr_dayOfWeek = expr.fields.dayOfWeek.toString();
+  const expr_dayOfMonth = expr.fields.dayOfMonth.toString();
+  const expr_month = expr.fields.month.toString();
+
+  const now_second = now.second().toString();
+  const now_minute = now.minute().toString();
+  const now_hour = now.hour().toString();
+  const now_day = now.day().toString();
+  const now_date = now.date().toString();
+  const now_month = now.month().toString();
+
+  return (
+    expr_second.includes(now_second) &&
+    expr_minute.includes(now_minute) &&
+    expr_hour.includes(now_hour) &&
+    expr_dayOfWeek.includes(now_day) &&
+    expr_dayOfMonth.includes(now_date) &&
+    expr_month.includes(now_month)
+  );
 }
 
 export function getIdleRunnerCount(scalingDownConfigs: ScalingDownConfigList): number {
